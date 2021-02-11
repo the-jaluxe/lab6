7cf41b6cfc866f07207bbb278e9769c837a89cf2
Jarrett Cornelison <jcornel00@gmail.com>
2021-02-10 23:13:27 -0800

    Final

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,30 +1,51 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-})
+$(document).ready(function () {
+  initializePage();
+});
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+  $('.project a').click(addProjectDetails);
 
-	// $('#colorBtn').click(randomizeColors);
+  // $('#colorBtn').click(randomizeColors);
 }
+
+function callBackFn() {}
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevent following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var projectID = $(this).closest('.project').attr('id');
+  // get rid of 'project' from the front of the id 'project3'
+  var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+  console.log('User clicked on project ' + idNumber);
+  let urlString = 'http://localhost:3000/project/' + String(idNumber);
+  console.log(urlString);
+  $.get(urlString, function (response) {
+    console.log(response);
+    let insertion = '#project' + idNumber + ' .details';
+    console.log(insertion);
+    let insertionText =
+      '<p>' +
+      response['title'] +
+      '</p>' +
+      '<p>' +
+      response['date'] +
+      '</p>' +
+      '<img src="' +
+      response['image'] +
+      '" class="detailsImage">' +
+      response['summary'];
+    $(insertion).html(insertionText);
+  });
 }
